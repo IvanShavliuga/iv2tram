123e5f939a762c4c7a4049fa7067e467763d083b
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2021-03-28 17:47:44 +0300

    added new design (pass unit)

---
+++ src/components/images/passunit.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="tram"
+    viewBox="0 0 30 65"
+    width="15"
+    height="25">
+    <circle
+      :cx="passX"
+      :cy="passY"
+      r="2.5"
+      fill="rgba(200, 200, 60, 0.6)"
+    />
+  </svg>
+</template>
+<script>
+export default {
+  props: {
+    passX: {
+      type: Number,
+      default: 0
+    },
+    passY: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
