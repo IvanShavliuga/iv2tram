c5887af1a3f30bcd744d8a976aef8efd8ea44da9
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2021-06-25 11:00:47 +0300

    new control panel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,18 +6,36 @@
     </div>
     <div class="control">
       <div class="control__panel">
-        <button @click="newtramcl">
-          new
-        </button>
-        <!-- <button @click="deltramcl">
-          delete
-        </button> -->
-        <button @click="prevtramcl">
-          prev
-        </button>
-        <button @click="nexttramcl">
-          next
-        </button>
+        <div class="control__panel-group">
+          trams:
+          <button @click="newtramcl">
+            new
+          </button>
+          <!-- <button @click="deltramcl">
+            delete
+          </button> -->
+          <button @click="prevtramcl">
+            prev
+          </button>
+          <button @click="nexttramcl">
+            next
+          </button>
+        </div>
+        <div class="control__panel-group">
+          window:
+          <button>
+            map
+          </button>
+          <!-- <button @click="deltramcl">
+            delete
+          </button> -->
+          <button>
+            depot
+          </button>
+          <button>
+            line
+          </button>
+        </div>
       </div>
     </div>
     <div class="box">
@@ -63,10 +81,14 @@ export default {
 
 <!-- Use preprocessors via the lang attribute! e.g. <style lang="scss"> -->
 <style>
-.page {
-  width: 90vw;
-  margin: auto;
+button {
+  border: 1px solid #238923;
+  background-color: rgba(50, 120, 90, 0.7);
+  color: white;
+  padding: 5px;
+  margin: 5px;
 }
+
 .tramline {
   position: relative;
   max-width: 100%;
@@ -86,12 +108,18 @@ export default {
   display: none;
 }
 .control__panel {
-  width: 250px;
+  display: flex;
+  width: 100%;
   margin: 15px;
+  padding: 0 15px;
+  background: rgba(60, 255, 255, 0.2);
+}
+.control__panel-group {
+  color: white;
 }
 .control {
-  display: none;
-  width: 250px;
+  display: block;
+  width: 100%;
   height: 60px;
 }
 .list {
@@ -115,34 +143,58 @@ export default {
     flex-direction: column;
   } */
 }
-@media (max-width: 535px) {
-  .page {
-    max-width: 80vw;
-    margin: 0;
+@media (max-width: 635px) {
+  .control {
+    width: 500px;
+    margin: 15px 0;
+    height: auto;
   }
+  .page {
+    width: 520px;
+    padding: 0 15px;
+    margin: 0 auto;
+  }
+}
+@media (max-width: 570px) {
   .tramline {
     max-width: 400px;
     padding: 0;
-    margin: 0 auto;
+    margin: 0 15px;
     margin-top: -30px;
   }
+  .page {
+    width: 500px;
+    padding: 0 5px;
+    margin: 0 auto;
+  }
+  .control__panel {
+    flex-direction: column;
+  }
+  .control__panel {
+    width: 250px;
+    margin: 10px auto;
+  }
 }
-@media (max-width: 400px) {
+@media (max-width: 510px) {
   .tramline {
     max-width: 200px;
     padding: 0;
-    margin: 0 auto;
-    margin-top: -30px;
-    transform: scaleX(1.3);
-  }
-}
-@media (max-width: 322px) {
-  .tramline {
-    max-width: 200px;
-    padding: 0;
-    margin: 0 auto;
+    margin: 0;
     margin-top: -30px;
     transform: scaleX(1.1);
+  }
+}
+@media (max-width: 432px) {
+  .tramline {
+    max-width: 200px;
+    padding: 0;
+    margin: 0 auto;
+    margin-top: -30px;
+    transform: scaleX(1.5) translateX(-60px);
+  }
+  .control__panel {
+    width: 250px;
+    margin: 10px;
   }
 }
 @media (max-width: 265px) {
