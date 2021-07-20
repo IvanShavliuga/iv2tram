015eaa66b354c52387d45b8bc7ecd733ec42195a
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2021-07-20 19:53:07 +0300

    fixed bag no-standart height

--- src/views/Way.vue
+++ src/views/Way.vue
@@ -7,7 +7,7 @@
 .map {
   position: relative;
   width: 750px;
-  height: 300px;
+  height: 250px;
   margin: 0 auto;
   background: rgba(255, 255, 255, 0.5);
   box-shadow: 0 0 5px 5px rgba(255, 255, 255, 0.5);
